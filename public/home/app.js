2f7cfe170f6fee70e25137753020114c443d2125
chirumer <chirumer@users.noreply.github.com>
2021-06-10 14:27:38 +0530

    added styling for closed page

--- public/home/app.js
+++ public/home/app.js
@@ -1,19 +1,24 @@
 (async function() {
 
-const start_quiz_button = document.getElementById('start_quiz');
-start_quiz_button.addEventListener('click', () => {
-    window.location.assign('/instructions')
-});
-const show_leaderboard_button = document.getElementById('show_leaderboard');
-show_leaderboard_button.addEventListener('click', () => {
-    window.location.assign('/leaderboard');
-});
-
 const response = await fetch('/get-quiz-timing');
 if (!response.ok) {
     console.log('failed to get quiz timing');
 }
 const { close_at } = await response.json();
+
+const start_quiz_button = document.getElementById('start_quiz');
+start_quiz_button.addEventListener('click', () => {
+    if (Date.now() >= close_at) {
+	window.location.assign('/closed');
+    }
+    else {
+	window.location.assign('/instructions')
+    }
+});
+const show_leaderboard_button = document.getElementById('show_leaderboard');
+show_leaderboard_button.addEventListener('click', () => {
+    window.location.assign('/leaderboard');
+});
 
 const answers_button = document.getElementById('show_answers');
 answers_button.addEventListener('click', () => {
